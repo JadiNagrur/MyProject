c1ab6af5d71411892538aa5b8718711af85057b6
JadiNagrur <jadinagrur@gmail.com>
2021-11-29 10:19:16 +0530

    Scene setup and Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        float moveHz = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHz, 0.0f, moveV);
+        rb.velocity = movement * speed;
+    }
+}
